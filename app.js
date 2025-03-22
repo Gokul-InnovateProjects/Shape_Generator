50decec220936e86da75d397e6382036e3666a55
Gokul-InnovateProjects <gokulmanivel73@gmail.com>
2025-03-22 23:07:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let button = document.getElementById("button");
+const wallet = document.getElementById("color");
+var numberInShape = 1;
+var oldvalue = 0;
+
+button.onclick = () => {
+  let n = document.getElementById("number").value;
+  const circle = document.getElementById("circle");
+  const square = document.getElementById("square");
+  const rectangle = document.getElementById("rectangle");
+  const box = document.getElementById("box");
+  
+  n = Number(n) + Number(oldvalue);
+
+  for (let j = numberInShape; j <= n; j++) {
+    let shape = document.createElement("div");
+    console.log(shape);
+    shape.innerHTML = numberInShape;
+
+    if (circle.checked) shape.classList.add("circle");
+    else if (square.checked) shape.classList.add("square");
+    else if (rectangle.checked) shape.classList.add("rectangle");
+    else document.write("Invalid Input");
+
+    numberInShape++;
+    box.appendChild(shape);
+
+    oldvalue = document.getElementById("box").lastElementChild.innerHTML;
+  }
+  function setWalletBackgroundColor(color) {
+    shape.style.backgroundColor = color;
+  }
+}
